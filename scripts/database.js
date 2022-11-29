25abf14d72c30c7fe0421637367ef48d50504f0f
Will Hedges <wshedges@outlook.com>
2022-11-29 11:05:41 -0600

    added shows

--- scripts/database.js
+++ scripts/database.js
@@ -1,7 +1,68 @@
 const database = {
-
-}
+  shows: [
+    {
+      id: 1,
+      actorId: 1,
+      genreId: 1,
+      name: "1899",
+    },
+    {
+      id: 2,
+      actorId: 2,
+      genreId: 1,
+      name: "Westworld",
+    },
+    {
+      id: 3,
+      actorId: 3,
+      genreId: 2,
+      name: "Parks and Recreation",
+    },
+    {
+      id: 4,
+      actorId: 4,
+      genreId: 2,
+      name: "Brooklyn 99",
+    },
+    {
+      id: 5,
+      actorId: 5,
+      genreId: 3,
+      name: "Peaky Blinders",
+    },
+    {
+      id: 6,
+      actorId: 6,
+      genreId: 3,
+      name: "Band of Brothers",
+    },
+    {
+      id: 7,
+      actorId: 7,
+      genreId: 4,
+      name: "House of the Dragon",
+    },
+    {
+      id: 8,
+      actorId: 8,
+      genreId: 4,
+      name: "The Witcher",
+    },
+    {
+      id: 9,
+      actorId: 9,
+      genreId: 5,
+      name: "Gangs of London",
+    },
+    {
+      id: 10,
+      actorId: 10,
+      genreId: 5,
+      name: "Lost",
+    },
+  ],
+};
 
 export const getShows = () => {
-  return null
-}
+  return null;
+};
