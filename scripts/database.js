744d087ad15597e01c97df688545d43f80fb9bc9
Logan Gray <loganallengray@gmail.com>
2022-11-29 11:18:29 -0600

    Added genre array to database

--- scripts/database.js
+++ scripts/database.js
@@ -1,7 +1,28 @@
 const database = {
-
+  genres: [{
+      id: 1,
+      name: "Action"
+    }, {
+      id: 2,
+      name: "Drama"
+    }, {
+      id: 3,
+      name: "Comedy"
+    }, {
+      id: 4,
+      name: "Horror"
+    }, {
+      id: 5,
+      name: "Western"
+    }
+  ]
 }
 
 export const getShows = () => {
   return null
+}
+
+// Returns copy of array filled with genre objects
+export const getGenres = () => {
+  return database.genres.map(genre => ({ ...genre }));
 }
