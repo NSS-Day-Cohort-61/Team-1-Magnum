ba4f787470c632ec70018a80bd8a3e2ef2ed1d9e
Matthew McDonald <yerwhile@gmail.com>
2022-11-29 10:59:44 -0600

    actors database

--- scripts/database.js
+++ scripts/database.js
@@ -1,5 +1,41 @@
 const database = {
+  actors: [{
+    id: 1,
+    name: "Bigfoot Bjornsen"
+  }, {
+    id: 2,
+    name: "Genghis Cohen"
+  }, {
+    id: 3,
+    name: "Mucho Maas"
+  }, {
+    id: 4,
+    name: "Oedipa Maas"
+  },
+  {
+    id: 5,
+    name: "Benny Profane"
+  }, {
+    id: 6,
+    name: "Tyrone Slothrop"
+  }, {
+    id: 7,
+    name: "Chevrolette McAdoo"
+  }, {
+    id: 8,
+    name: "Rachel Owlglass"
+  }, {
+    id: 9,
+    name: "Roger Mexico"
+  }, {
+    id: 10,
+    name: "Horst Achtfaden"
+  }]
 
+}
+
+export const getActors = () => {
+  return database.actors.map(actor => ({...actor}));
 }
 
 export const getShows = () => {
