f9d6229ad515ac960aafe35bd8e2332b95639949
Will Hedges <wshedges@outlook.com>
2022-12-01 10:21:48 -0600

    changed Shows html component name

--- scripts/shows.js
+++ scripts/shows.js
@@ -1,9 +1,23 @@
-import { getShows } from "./database";
+import { getActors, getShows } from "./database.js";
 
+const actors = getActors();
 const shows = getShows();
 
-export const showsHTML = () => {
-  let html = "";
-
+export const Shows = () => {
+  let html = "<ul>";
+  // go through each show
+  for (const show of shows) {
+    // need to take the foreign key of the show actorId
+    //  and match to the actor id primary key
+    let starActor = null;
+    for (const actor of actors) {
+      if (show.actorId === actor.id) {
+        starActor = actor;
+      }
+    }
+    // build and add a list item string to the html string
+    html += `<li>${show.name} (${show.genre}) starring ${starActor}</li>`;
+  }
+  html += "</ul>";
   return html;
 };
